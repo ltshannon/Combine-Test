b83eefd191bea19708eebe952cc23577ec806515
Larry Shannon <larry@breakawaydesign.com>
2021-02-17 15:02:32 -0800

    Initial Commit

---
+++ Combine Test/Combine_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_TestApp.swift
+//  Combine Test
+//
+//  Created by Larry Shannon on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Combine_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
